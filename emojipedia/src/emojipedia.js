e4dd1afacb520fac90910c09a173b321a928e60e
Kurt F <67562815+fk-pixel@users.noreply.github.com>
2021-10-01 01:48:02 +0200

    Add files via upload

---
+++ emojipedia/src/emojipedia.js
@@ -0,0 +1,83 @@
+const emojipedia = [
+    {
+      id: 1,
+      emoji: "💪",
+      name: "Tense Biceps",
+      meaning:
+        "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+    },
+    {
+      id: 2,
+      emoji: "🙏",
+      name: "Person With Folded Hands",
+      meaning:
+        "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+    },
+    {
+      id: 3,
+      emoji: "🤣",
+      name: "Rolling On The Floor, Laughing",
+      meaning:
+        "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+    },
+    {
+      id: 4,
+      emoji: "🥺",
+      name: "Pleading Face",
+      meaning:"A classic smiley, turned upside down. Implemented as a flipped version of 🙂 Slightly Smiling Face on most platforms. Commonly used to convey irony, sarcasm, joking, or a sense of goofiness or silliness. Its intent can be similar to the bemused Oh well! of 🤷 Person Shrugging or the shruggie emoticon, ¯\_(ツ)_/¯."},
+    {
+        id: 5,
+        emoji: "🙃",
+        name: "Upside-Down Face",
+        meaning: "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+    },
+    {
+        id: 6,
+        emoji: "😉",
+        name: "Winking Face",
+        meaning: "A yellow face with a slight smile shown winking, usually its left eye. May signal a joke, flirtation, hidden meaning, or general positivity. Tone varies, including playful, affectionate, suggestive, or ironic."
+    },
+    {
+        id: 7,
+        emoji: "😇",
+        name: "Smiling Face with Halo",
+        meaning: "A yellow face with smiling eyes, closed smile, and halo, usually blue, overhead. Often represents angels, prayers, and blessings. May also convey angelic behavior, e.g., doing good deeds. See also 👼 Baby Angel."
+    },
+    {
+        id: 8,
+        emoji: "😜",
+        name: "Winking Face with Tongue",
+        meaning:"A yellow face sticking out its tongue and making a zany wink, usually shown with its right eye closed and left wide open. Often conveys a sense of fun, excitement, wackiness, buffoonery, or joking."
+    },
+    {
+        id: 9,
+        emoji: "🤑",
+        name: "Money-Mouth Face",
+        meaning:
+          "A yellow face with raised eyebrows, dollar signs for eyes, and an open smile sticking out a tongue styled after a green, dollar banknote. Some platforms depict the dollar-sign eyes in green circles." 
+    },
+    {
+        id: 10,
+        emoji: "🤐",
+        name: "Zipper-Mouth Face",
+        meaning:
+        "A yellow face with simple, open eyes and a closed zipper for a mouth. Meaning widely varies, but commonly conveys a secret or that someone will keep one (e.g., My lips are sealed). May also be used to tell someone to stop talking (e.g., Zip it! or I’ll shut up now)."
+    },
+    {
+        id: 11,
+        emoji: "😐",
+        name: "Neutral Face",
+        meaning:
+        "A yellow face with simple, open eyes and a flat, closed mouth. Intended to depict a neutral sentiment but often used to convey mild irritation and concern or a deadpan sense of humor."
+    },
+    {
+        id: 12,
+        emoji: "🧐",
+        name: "Face with Monocle",
+        meaning:
+          "“A yellow face with furrowed eyebrows wearing a monocle. Usually depicted with a small, intent frown and head slightly upturned, as if in careful inspection. May show that someone is pondering, considering, or questioning something, sometimes with a sense of skeptical or ironic observation or encouraging a closer look at some content. May convey a sense of sophistication or smug intelligence, e.g., smarty-pants."
+    }
+  ];
+  
+  export default emojipedia;
+  
